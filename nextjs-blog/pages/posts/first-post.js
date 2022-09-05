3029361ef8e921916e625ad0e48351cf42680797
alekspanteli <alekspanteli@rambler.ru>
2022-09-05 12:52:08 +0000

    Update 7 files

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -19,9 +19,6 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
       <ShowProfileImage />
     </Layout>
   );
